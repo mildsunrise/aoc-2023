cd3aa83f8268aed91667775b3b883acaebe49277
Alba Mendez <me@alba.sh>
2023-12-29 15:48:56 +0100

    AoC 24 part 2

--- p24.hs
+++ p24.hs
@@ -13,17 +13,20 @@ parts x = (part1 x, part2 x)
 
 -- GENERIC ALGORITHMS
 
-solve n xs =
-  iterate (>>= solveStep) (Just ([], xs)) !! n
+solve k xs =
+  fmap (map (\[a] -> a) . fst) $
+  iterate (>>= solveStep) (Just ([], xs)) !! k
 
 solveStep (as, xs) = do
   (bs, xs) <- pure $ span ((== 0) . head) xs
-  (k:c, xs) <- uncons xs
-  c <- pure $ map (* (1/k)) c
-  let f (k:r) = zipWith (-) r $ map (* k) c
-  pure (map f as ++ [c], map f (bs ++ xs))
+  (k:p, xs) <- uncons xs
+  p <- pure $ map (* (1/k)) p
+  let f (k:r) = zipWith (-) r $ map (* k) p
+  pure (map f as ++ [p], map f (bs ++ xs))
 
 pairs xs = [(x, y) | (x:ys) <- tails xs, y <- ys]
+
+det2 [[a, b], [c, d]] = a * d - b * c
 
 
 -- PARSING
@@ -41,7 +44,7 @@ inTestArea x = 2 * 10^14 <= x && x <= 4 * 10^14
 
 isIntersecting ((pa, va), (pb, vb)) = isJust $ do
   let mat = [va, map negate vb, zipWith (-) pb pa]
-  ([[ka], [kb]], []) <- solve 2 (transpose mat)
+  [ka, kb] <- solve 2 (transpose mat)
   let point = zipWith (+) pa $ map (* ka) va
   guard $ ka >= 0 && kb >= 0 && all inTestArea point
 
@@ -56,4 +59,26 @@ part1 =
 
 -- PART 2
 
-part2 = const "TODO"
+choose2 [a, b, c] = [
+  (\[x, y] -> [x, y, 0], [a, b]),
+  (\[x, y] -> [x, 0, y], [a, c]),
+  (\[x, y] -> [0, x, y], [b, c])]
+
+minors2 = map (det2 . snd) . choose2 . transpose
+
+produceEqs (pb, vb) (pc, vc) = let
+  cols = zip (zipWith (-) vb vc) (zipWith (-) pc pb)
+  ks = zipWith (-) (minors2 [pb, vb]) (minors2 [pc, vc])
+  in zipWith produceEq ks $ choose2 cols
+
+produceEq k (build, rows) = let
+  f [a, b] = build [b, -a]
+  (vd, pd) = unzip rows
+  in f vd ++ f pd ++ [k]
+
+part2 =
+  sum .
+  (\(Just xs) -> take 3 xs) .
+  solve 6 .
+  (\(x:xs) -> concatMap (produceEqs x) xs) .
+  map (parseLine rationalVec)
